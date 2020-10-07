1a51bb5017423c68352bd46dc1346f4c5ced0575
DevOps-Pipeline-Practice <72354687+DevOps-Pipeline-Practice@users.noreply.github.com>
2020-10-08 02:14:07 +0530

    Delete hello.sh

--- hello.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-##################################################################
-echo "hello world"
-echo "welcome to jenkins - github pipeline"
-echo "new line added"
-echo " pushing changes from ec2-server"
